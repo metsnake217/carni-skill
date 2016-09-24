5ca8c834ffa4e16a40a9b8ebdae7341532ea6310
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-09-24 11:55:46 -0400

    adding schedule test with db connection

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var Scheduler = schedulerClass.Scheduler;
 
 
 app.launch( function( request, response ) {
-	response.say( 'Hello, welcome to Carnival. Now that we are friends, I can tell you a secret. I will be your personal assistant on this cruise and will get you information about which activities are happening at what time, where you should eat or drink, wake you up with your favorite music in the morning, and even turn your lights on and off. Try me, say: "Alexa turn lights on" or "Alexa turn lights off". For the full list of commands look at the brochure behind me.' ).reprompt( 'By the way: I can still say numbers from 1 to 100!' ).shouldEndSession( false );
+	response.say( 'Hello, welcome to Carnival. Now that we are friends, I can tell you a secret. I will be your personal assistant on this cruise and will get you information about which activities are happening at what time, where you should eat or drink, wake you up with your favorite music in the morning, and even turn your lights on and off. Try me, say: "Alexa turn lights on" or "Alexa turn lights off" or "Alexa What is the schedule by the Pool today". For the full list of commands look at the brochure behind me.' ).reprompt( 'By the way: I can still say numbers from 1 to 100!' ).shouldEndSession( false );
     } );
 
 
