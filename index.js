5d0dfeb49db264098dab63b647f8a1a548b30b5b
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-09-25 18:52:03 -0400

    adding schedule test with db connection

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ app.error = function( exception, request, response ) {
     response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.launch( function( request, response ) {
+app.launch( function( request, response, exception ) {
 	//response.say( 'Hello, welcome to Carnival. Now that we are friends, I can tell you a secret. I will be your personal assistant on this cruise and will get you information about which activities are happening at what time, where you should eat or drink, wake you up with your favorite music in the morning, and even turn your lights on and off. Try me, say: "Alexa turn lights on" or "Alexa turn lights off" or "Alexa What is the schedule by the Pool today". For the full list of commands look at the brochure behind me.' ).reprompt( 'Are you still there?' ).shouldEndSession( false );
-	response.say( 'Hello, welcome to Carnival.').reprompt( 'Are you still there? Would you like to know the schedule by a specific location today? Say Alexa what is the schedule by the location today?' ).shouldEndSession( true );
+	response.say( 'Hello, welcome to Carnival.').reprompt( 'Are you still there? Would you like to know the schedule by a specific location today? Say Alexa what is the schedule by the location today?' );
+	response.shouldEndSession( false );
+	console.log("exception  is " + exception);
     } );
 /*console.log("requesting alexa now");
 app.request({
