6e8e9370124e0e88f3ced271b524902e17e4565c
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-09-25 20:02:22 -0400

    adding schedule test with db connection

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.intent('closeSessionQuestion',
 app.intent('carniHelp',
 	   {},
 	   function(request,response) {
-	       		response.say("Hello there! I am your personal assistant on this cruise and will get you information about which activities are happening at what time, where you should eat or drink, wake you up with your favorite music in the morning, and even turn your lights on and off. Try me, say: 'Alexa turn lights on' or 'Alexa turn lights off' or 'Alexa What is the schedule by the Pool today'. For the full list of commands look at the brochure behind me.'");
+	       		response.say("Sure. Hello there! I am your personal assistant on this cruise and will get you information about which activities are happening at what time, where you should eat or drink, wake you up with your favorite music in the morning, and even turn your lights on and off. Try me, say: 'Alexa All schedules today'. or. 'Alexa Pool today'. or. 'Alexa What\'s the schedule by the Pool tomorrow?'. or 'Alexa stop'. For the full list of commands look at the brochure behind me.'").reprompt("I hope these helped! what can i do you for?");
 	       		response.shouldEndSession(false);
 	   }
 );
